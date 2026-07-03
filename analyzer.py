2fb2a871069113adc4252338979ec85e9e3679c9
angrey30111986 <angrey30111986@gmail.com>
2026-07-03 17:48:56 +0300

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -2,6 +2,8 @@ def analyze_match(home, away):
     print(f"Аналіз матчу: {home} - {away}")
 
     return {
+        "team1": home,
+        "team2": away,
         "winner": home,
         "confidence": 72,
         "prediction": "П1"
