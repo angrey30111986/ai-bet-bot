66eee9481253e74781937f358552a36386c3dfec
angrey30111986 <angrey30111986@gmail.com>
2026-07-03 17:54:18 +0300

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,10 +1,22 @@
+import random
+
 def analyze_match(home, away):
+    confidence = random.randint(55, 90)
+
+    if confidence >= 75:
+        prediction = "П1"
+        winner = home
+    elif confidence >= 65:
+        prediction = "X"
+        winner = "Нічия"
+    else:
+        prediction = "П2"
+        winner = away
+
     print(f"Аналіз матчу: {home} - {away}")
 
     return {
-        "team1": home,
-        "team2": away,
-        "winner": home,
-        "confidence": 72,
-        "prediction": "П1"
+        "winner": winner,
+        "confidence": confidence,
+        "prediction": prediction
     }
