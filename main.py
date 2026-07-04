df3ce4e063fab35453f7ebf35584714b3debe8f9
angrey30111986 <angrey30111986@gmail.com>
2026-07-04 14:34:48 +0300

    Print league and match details from response data

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ from analyzer import get_matches
 
 data = get_matches()
 
-print(data)
+for match in data["response"]:
+    home = match["teams"]["home"]["name"]
+    away = match["teams"]["away"]["name"]
+    league = match["league"]["name"]
+
+    print(f"{league}")
+    print(f"{home} - {away}")
+    print("----------------")
