6723c7ca64b6f9886af01d6873adc2462cfd3508
angrey30111986 <angrey30111986@gmail.com>
2026-07-04 14:30:46 +0300

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -3,8 +3,7 @@ from config import API_KEY
 
 def get_matches():
 
-    url = "https://v3.football.api-sports.io/status"
-
+    url = "https://v3.football.api-sports.io/fixtures?date=2026-07-04"
     headers = {
         "x-apisports-key": API_KEY
     }
