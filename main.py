64807310eb7823549db10a98a6162c61c4f77bfc
angrey30111986 <angrey30111986@gmail.com>
2026-07-04 14:42:30 +0300

    Display all match scores and predictions
    Updated match display to show all matches and their scores, including home and away team goals and predictions.

--- main.py
+++ main.py
@@ -2,14 +2,25 @@ from analyzer import get_matches
 
 data = get_matches()
 
-for match in data["response"][:5]:
+for match in data["response"]:
 
     home = match["teams"]["home"]["name"]
     away = match["teams"]["away"]["name"]
 
-    print(f"{home} - {away}")
+    hg = match["goals"]["home"]
+    ag = match["goals"]["away"]
 
-    print("Прогноз: П1")
-    print("Ймовірність: 72%")
-    print("Тотал: Більше 2.5")
-    print("-------------------")
+    print("---------------------")
+    print(home, "-", away)
+    print("Рахунок:", hg, ":", ag)
+
+    if hg is not None and ag is not None:
+
+        if hg > ag:
+            print("Лідирує домашня команда")
+
+        elif ag > hg:
+            print("Лідирує гості")
+
+        else:
+            print("Нічия")
