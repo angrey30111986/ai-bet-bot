e5722cf51403c2d4da986ac7151544983eca9870
angrey30111986 <angrey30111986@gmail.com>
2026-07-04 14:38:01 +0300

    Limit match output to first 5 entries

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from analyzer import get_matches
 
 data = get_matches()
 
-for match in data["response"]:
+for match in data["response"][:5]:
+
     home = match["teams"]["home"]["name"]
     away = match["teams"]["away"]["name"]
-    league = match["league"]["name"]
 
-    print(f"{league}")
     print(f"{home} - {away}")
-    print("----------------")
+
+    print("Прогноз: П1")
+    print("Ймовірність: 72%")
+    print("Тотал: Більше 2.5")
+    print("-------------------")
