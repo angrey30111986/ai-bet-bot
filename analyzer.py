6621ac97c8c2870fc37fce43bf28b7135fabd1b6
angrey30111986 <angrey30111986@gmail.com>
2026-07-04 14:14:38 +0300

    Change API URL to fetch status instead of fixtures

--- analyzer.py
+++ analyzer.py
@@ -3,7 +3,7 @@ from config import API_KEY
 
 def get_matches():
 
-    url = "https://v3.football.api-sports.io/fixtures?next=5"
+    url = "https://v3.football.api-sports.io/status"
 
     headers = {
         "x-apisports-key": API_KEY
