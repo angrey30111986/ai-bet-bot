70840d0a809ca24e5c45e53208184230393bc861
angrey30111986 <angrey30111986@gmail.com>
2026-07-06 22:36:26 +0300

    Update injuries.py

--- v2/v2/injuries.py
+++ v2/v2/injuries.py
@@ -1,17 +1,45 @@
 import requests
 
-API_KEY = "ТВІЙ_API_KEY"
+API_KEY = "03b997d33e6e88f7b1dc3e9f179d7f17"
 
 headers = {
     "x-apisports-key": API_KEY
 }
 
+
 def get_injuries(fixture_id):
-    url = f"https://v3.football.api-sports.io/injuries?fixture={fixture_id}"
 
-    r = requests.get(url, headers=headers)
+    url = "https://v3.football.api-sports.io/injuries"
 
-    if r.status_code != 200:
+    params = {
+        "fixture": fixture_id
+    }
+
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params
+    )
+
+    if response.status_code != 200:
+        print("API Error:", response.status_code)
         return []
 
-    return r.json()["response"]
+    data = response.json()
+
+    if "response" not in data:
+        return []
+
+    return data["response"]
+
+
+if __name__ == "__main__":
+
+    fixture_id = 1035039  # Для тесту заміни на потрібний ID матчу
+
+    injuries = get_injuries(fixture_id)
+
+    print("Кількість травм:", len(injuries))
+
+    for player in injuries:
+        print(player)
