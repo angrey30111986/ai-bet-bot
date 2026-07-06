60fa242b07adb6cea6e1e76c8837007b5f45ab76
angrey30111986 <angrey30111986@gmail.com>
2026-07-06 19:15:53 +0300

    Update dataset.py

--- v2/dataset.py
+++ v2/dataset.py
@@ -15,37 +15,35 @@ def build_dataset(df):
         home = match["home_team"]
         away = match["away_team"]
 
-        home_rest = days_since_last(home, match["date"])
-        away_rest = days_since_last(away, match["date"])
-
         home_elo = get(home)
         away_elo = get(away)
 
+        home_rest = days_since_last(home, match["date"])
+        away_rest = days_since_last(away, match["date"])
+
         if match["home_goals"] > match["away_goals"]:
             result = 0
-
-        elif match["home_goals"] == match["away_goals"]:
+        elif match["home_goals"] < match["away_goals"]:
+            result = 2
+        else:
             result = 1
 
-        else:
-            result = 2
-
         rows.append({
+            "home_team": home,
+            "away_team": away,
 
             "home_elo": home_elo,
-
             "away_elo": away_elo,
-
-            "elo_diff": home_elo-away_elo,
+            "elo_diff": home_elo - away_elo,
 
             "home_rest": home_rest,
-
             "away_rest": away_rest,
+            "rest_diff": home_rest - away_rest,
 
-            "rest_diff": home_rest-away_rest,
+            "home_goals": match["home_goals"],
+            "away_goals": match["away_goals"],
 
             "result": result
-
         })
 
         update(home, away, result)
