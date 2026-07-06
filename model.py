4a1b622e46a13825fc093bfc42e28883993397df
angrey30111986 <angrey30111986@gmail.com>
2026-07-06 18:33:17 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,12 @@
+import joblib
+
+model = joblib.load("football_ai.pkl")
+
 def predict(features):
+    probs = model.predict_proba([features])[0]
 
     return {
-        "home":65,
-        "draw":20,
-        "away":15
+        "home": round(probs[0] * 100, 1),
+        "draw": round(probs[1] * 100, 1),
+        "away": round(probs[2] * 100, 1)
     }
