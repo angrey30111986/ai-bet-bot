961e1669a4e596d768d77193114975442e76ff6f
angrey30111986 <angrey30111986@gmail.com>
2026-07-06 15:43:43 +0300

    Refactor dataset.py to create dataset.csv
    Removed API requests and match data processing. Created a DataFrame with specified columns and saved it as 'dataset.csv'.

--- dataset.py
+++ dataset.py
@@ -1,41 +1,23 @@
-import requests
 import pandas as pd
-from config import API_KEY
 
-headers = {
-    "x-apisports-key": API_KEY
-}
+columns = [
+    "home_team",
+    "away_team",
+    "home_elo",
+    "away_elo",
+    "home_form",
+    "away_form",
+    "home_attack",
+    "away_attack",
+    "home_defense",
+    "away_defense",
+    "home_goals_avg",
+    "away_goals_avg",
+    "result"
+]
 
-rows = []
+df = pd.DataFrame(columns=columns)
 
-for season in [2023, 2024, 2025]:
-    print(f"Сезон {season}")
+df.to_csv("dataset.csv", index=False)
 
-    response = requests.get(
-        "https://v3.football.api-sports.io/fixtures",
-        headers=headers,
-        params={
-            "league": 39,
-            "season": season
-        }
-    )
-
-    data = response.json()["response"]
-
-    for match in data:
-
-        rows.append({
-
-            "home_goals": match["goals"]["home"],
-            "away_goals": match["goals"]["away"],
-
-            "result":
-                1 if match["goals"]["home"] > match["goals"]["away"]
-                else 0 if match["goals"]["home"] == match["goals"]["away"]
-                else 2
-
-        })
-
-pd.DataFrame(rows).to_csv("matches.csv", index=False)
-
-print("matches.csv створено")
+print("dataset.csv створено")
