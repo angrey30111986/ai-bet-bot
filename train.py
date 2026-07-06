00313d7f95475ddcccf9ccd8e8d0548afb889ed4
angrey30111986 <angrey30111986@gmail.com>
2026-07-06 15:15:17 +0300

    Implement training of CatBoost model for football data

---
+++ train.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import joblib
+
+from catboost import CatBoostClassifier
+
+from features import create_features
+
+df = pd.read_csv("matches.csv")
+
+df = create_features(df)
+
+X = df[[
+    "goal_diff",
+    "total_goals"
+]]
+
+y = df["result"]
+
+model = CatBoostClassifier(
+    iterations=500,
+    learning_rate=0.05,
+    depth=8,
+    verbose=False
+)
+
+model.fit(X, y)
+
+joblib.dump(model, "football_ai.pkl")
+
+print("AI готовий")
