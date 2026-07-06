19201ef3740d821039689423f8f142af8bf190b6
angrey30111986 <angrey30111986@gmail.com>
2026-07-06 19:00:48 +0300

    Update trainer.py

--- v2/trainer.py
+++ v2/trainer.py
@@ -4,27 +4,35 @@ import joblib
 from catboost import CatBoostClassifier
 from features import create_features
 
+print("Loading matches...")
+
 df = pd.read_csv("matches.csv")
 
 df = create_features(df)
 
-X = df[[
-    "goal_diff",
-    "total_goals"
-]]
+features = [
+    "home_form",
+    "away_form",
+    "home_attack",
+    "away_attack"
+]
+
+X = df[features].fillna(0)
 
 y = df["result"]
 
+print("Training AI...")
+
 model = CatBoostClassifier(
-    iterations=500,
-    learning_rate=0.05,
+    iterations=1000,
+    learning_rate=0.03,
     depth=8,
     loss_function="MultiClass",
-    verbose=False
+    verbose=100
 )
 
 model.fit(X, y)
 
 joblib.dump(model, "football_ai.pkl")
 
-print("Модель навчена.")
+print("AI model saved.")
