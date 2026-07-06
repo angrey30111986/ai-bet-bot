6c3ce27b56547aa65c81b28ae8af66a6f4582ce7
angrey30111986 <angrey30111986@gmail.com>
2026-07-06 15:46:21 +0300

    Update collector.py

--- collector.py
+++ collector.py
@@ -1,82 +1,54 @@
 import requests
-import pandas as pd
-import time
 from config import API_KEY
 
 headers = {
     "x-apisports-key": API_KEY
 }
 
-# Ліги
-leagues = [
-    39,   # Англія
-    140,  # Іспанія
-    135,  # Італія
-    78,   # Німеччина
-    61,   # Франція
-    94,   # Португалія
-    88,   # Нідерланди
-    203,  # Туреччина
-    307,  # Саудівська Аравія
-    71    # Бразилія
-]
 
-rows = []
+def get_matches(league, season):
 
-for league in leagues:
+    url = f"https://v3.football.api-sports.io/fixtures?league={league}&season={season}"
 
-    for season in [2022, 2023, 2024, 2025]:
+    r = requests.get(url, headers=headers)
 
-        print(f"Ліга {league} Сезон {season}")
+    return r.json()["response"]
 
-        r = requests.get(
-            "https://v3.football.api-sports.io/fixtures",
-            headers=headers,
-            params={
-                "league": league,
-                "season": season
-            }
-        )
 
-        data = r.json()["response"]
+if __name__ == "__main__":
 
-        for match in data:
+    leagues = [
+        39,
+        140,
+        78,
+        61,
+        94,
+        135,
+        253,
+        71,
+        144,
+        88
+    ]
 
-            if match["goals"]["home"] is None:
-                continue
+    seasons = [
+        2022,
+        2023,
+        2024,
+        2025
+    ]
 
-            rows.append({
+    total = 0
 
-                "league": league,
+    for league in leagues:
+        for season in seasons:
 
-                "season": season,
+            print(f"Ліга {league} Сезон {season}")
 
-                "home": match["teams"]["home"]["name"],
+            matches = get_matches(league, season)
 
-                "away": match["teams"]["away"]["name"],
+            print("Матчів:", len(matches))
 
-                "home_goals": match["goals"]["home"],
+            total += len(matches)
 
-                "away_goals": match["goals"]["away"],
-
-                "result":
-                    1 if match["goals"]["home"] >
-                    match["goals"]["away"]
-
-                    else 0 if
-                    match["goals"]["home"] ==
-                    match["goals"]["away"]
-
-                    else 2
-
-            })
-
-        time.sleep(1)
-
-df = pd.DataFrame(rows)
-
-df.to_csv("matches.csv", index=False)
-
-print(df.head())
-
-print(len(df))
+    print()
+    print("Всього матчів:", total)
