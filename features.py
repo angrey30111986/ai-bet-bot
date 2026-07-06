27be38997b067c3252d9a398e103f5a17b297fb5
angrey30111986 <angrey30111986@gmail.com>
2026-07-06 15:50:41 +0300

    Update features.py

--- features.py
+++ features.py
@@ -1,21 +1,2 @@
-from elo import get
-
-from form import form
-
-def features(home, away):
-
-    return {
-
-        "home_elo": get(home),
-
-        "away_elo": get(away),
-
-        "elo_diff": get(home)-get(away),
-
-        "home_form": form(home),
-
-        "away_form": form(away),
-
-        "form_diff": form(home)-form(away)
-
-    }
+def create_features(df):
+    return df
