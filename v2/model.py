0afbaf52ac12ed938cef353f4fc6d2e4c8aa4f9b
angrey30111986 <angrey30111986@gmail.com>
2026-07-07 22:56:48 +0300

    Refactor model file path and add existence checks
    Refactor model file path definition and add print statements to check model file existence.

--- v2/model.py
+++ v2/model.py
@@ -2,22 +2,12 @@ import os
 import joblib
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_FILE = os.path.join(BASE_DIR, "models", "football_ai.pkl")
 
-MODEL_FILE = os.path.join(
-    BASE_DIR,
-    "models",
-    "football_ai.pkl"
-)
-
+print("MODEL PATH:", MODEL_FILE)
+print("EXISTS:", os.path.exists(MODEL_FILE))
 
 def load_model():
-
     if not os.path.exists(MODEL_FILE):
         return None
-
     return joblib.load(MODEL_FILE)
-
-
-def save_model(model):
-
-    joblib.dump(model, MODEL_FILE)
