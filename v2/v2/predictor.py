b2f4d934519abcca96bb4c69cac3110d7a812dd5
angrey30111986 <angrey30111986@gmail.com>
2026-07-07 13:43:54 +0300

    Create predictor.py

---
+++ v2/v2/predictor.py
@@ -0,0 +1,7 @@
+def predict_match(home, away):
+    return {
+        "home": home,
+        "away": away,
+        "prediction": "Переможець поки не розрахований",
+        "confidence": "0%"
+    }
