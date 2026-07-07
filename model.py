4b125eef58a40dbc102e7248a2039b1a25c2866a
angrey30111986 <angrey30111986@gmail.com>
2026-07-07 22:51:22 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,12 +1,13 @@
+import os
 import joblib
 
-model = joblib.load("football_ai.pkl")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_FILE = os.path.join(BASE_DIR, "models", "football_ai.pkl")
 
-def predict(features):
-    probs = model.predict_proba([features])[0]
 
-    return {
-        "home": round(probs[0] * 100, 1),
-        "draw": round(probs[1] * 100, 1),
-        "away": round(probs[2] * 100, 1)
-    }
+def load_model():
+    if not os.path.exists(MODEL_FILE):
+        return None
+
+    with open(MODEL_FILE, "rb") as f:
+        return joblib.load(f)
