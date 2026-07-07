467d0837d2db8b437cd378a1f54a2ec5f32e71e1
angrey30111986 <angrey30111986@gmail.com>
2026-07-07 19:42:20 +0300

    Implement model loading and saving functions
    Added functions to load and save a machine learning model using joblib.

--- v2/model.py
+++ v2/model.py
@@ -1,1 +1,23 @@
-# TODO
+import os
+import joblib
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+MODEL_FILE = os.path.join(
+    BASE_DIR,
+    "models",
+    "football_ai.pkl"
+)
+
+
+def load_model():
+
+    if not os.path.exists(MODEL_FILE):
+        return None
+
+    return joblib.load(MODEL_FILE)
+
+
+def save_model(model):
+
+    joblib.dump(model, MODEL_FILE)
