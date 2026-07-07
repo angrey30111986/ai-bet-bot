dbf9b770b6eafe11d95b7f7cc35cac88024ead04
angrey30111986 <angrey30111986@gmail.com>
2026-07-07 13:44:12 +0300

    Delete v2/v2 directory

--- v2/v2/predictor.py
+++
@@ -1,7 +0,0 @@
-def predict_match(home, away):
-    return {
-        "home": home,
-        "away": away,
-        "prediction": "Переможець поки не розрахований",
-        "confidence": "0%"
-    }
