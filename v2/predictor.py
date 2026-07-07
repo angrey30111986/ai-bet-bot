5ec7c6e04dbab5cfccdd730529d0f35dfe7d3260
angrey30111986 <angrey30111986@gmail.com>
2026-07-07 20:41:29 +0300

    Update predictor.py

--- v2/predictor.py
+++ v2/predictor.py
@@ -1,40 +1,33 @@
+import pandas as pd
+
 from collector import collect_data
+from model import load_model
 
 
 def predict(home_team, away_team, fixture_id=None):
 
+    model = load_model()
+
+    if model is None:
+        raise Exception("Model not trained")
+
     data = collect_data(home_team, away_team, fixture_id)
 
-    home = 50
+    X = pd.DataFrame([{
+        "home_elo": data["home_elo"],
+        "away_elo": data["away_elo"],
+        "elo_diff": data["elo_diff"],
+        "home_rest": data["home_rest_days"],
+        "away_rest": data["away_rest_days"],
+        "rest_diff": data["home_rest_days"] - data["away_rest_days"],
+        "home_injuries": data["home_injuries"],
+        "away_injuries": data["away_injuries"]
+    }])
 
-    if data.get("elo_diff", 0) > 100:
-        home += 10
-
-    if data.get("home_rest_days", 0) > data.get("away_rest_days", 0):
-        home += 5
-
-    if data.get("home_injuries", 0) < data.get("away_injuries", 0):
-        home += 5
-
-    temperature = data.get("temperature")
-
-    if isinstance(temperature, (int, float)):
-        if temperature > 30:
-            home -= 2
-
-    wind = data.get("wind")
-
-    if isinstance(wind, (int, float)):
-        if wind > 25:
-            home -= 2
-
-    home = max(5, min(90, home))
-
-    draw = 25
-    away = 100 - home - draw
+    proba = model.predict_proba(X)[0]
 
     return {
-        "home": home,
-        "draw": draw,
-        "away": away
+        "home": round(proba[2] * 100),
+        "draw": round(proba[1] * 100),
+        "away": round(proba[0] * 100)
     }
